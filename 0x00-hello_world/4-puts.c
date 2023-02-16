4185d5ab73f51d59b8ec3bebd3d8afd3f533fa37
root <okeibrahim917@gmail.com>
2023-02-16 08:55:12 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,14 +7,10 @@
  * Return: 0 (success)
  */
  
-
-
  int main(void)
  
- {
+{
+  puts("\"Programming is like building a multilingual puzzle");
 
- 	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+  return (0);
 }
